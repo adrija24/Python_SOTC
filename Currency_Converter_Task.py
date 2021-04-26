0b11f6f598b238bbf473eb10924c9794716a1d3c
Aritra Roy <aritraroy24@gmail.com>
2021-04-26 23:04:32 +0530

    previous program issue fixed and task added

--- Currency_Converter_Task.py
+++ Currency_Converter_Task.py
@@ -1,21 +1,21 @@
 # Day - 2 === Currency Converter from INR to other
 
-USD = float(74.22)  # United States Dollar
-EUR = float(90.43)  # Euro
-JPY = float(0.69)   # Japanese Yen
-GBP = float(104.0)  # Great Britain Pound
-CAD = float(59.97)  # Canadian Dollars
+INR = float(0.013)  # United States Dollar
+EUR = float(1.21)  # Euro
+JPY = float(0.0093)   # Japanese Yen
+GBP = float(1.39)  # Great Britain Pound
+CAD = float(0.81)  # Canadian Dollars
 
 result = None
 
 amount = float(input("Enter the Amount: "))
-currency = input("Enter currency (USD, EUR, JPY, GBP, CAD): ")
+currency = input("Enter currency (INR, EUR, JPY, GBP, CAD): ")
 convert = input(
-    f"Do you want to convert from INR to {currency} or {currency} to INR (Press 1 & 2 for respective options): ")
+    f"Do you want to convert from USD to {currency.upper()} or {currency.upper()} to USD (Press 1 & 2 for respective options): ")
 
 if convert == "1": # INR to other
-    if currency == "USD" or currency == "usd":
-        result = amount / USD
+    if currency == "INR" or currency == "inr":
+        result = amount / INR
     elif currency == "EUR" or currency == "eur":
         result = amount / EUR
     elif currency == "JPY" or currency == "jpy":
@@ -28,11 +28,11 @@ if convert == "1": # INR to other
     if result is None:
         print("Invalid Currency")
     else:
-        print(f"{amount} INR = {result} {currency}")
+        print(f"{amount} USD = {result} {currency.upper()}")
 
 elif convert=="2": # INR from other
-    if currency == "USD" or currency == "usd":
-        result = amount * USD
+    if currency == "INR" or currency == "inr":
+        result = amount * INR
     elif currency == "EUR" or currency == "eur":
         result = amount * EUR
     elif currency == "JPY" or currency == "jpy":
@@ -45,7 +45,7 @@ elif convert=="2": # INR from other
     if result is None:
         print("Invalid Currency")
     else:
-        print(f"{amount} {currency} = {result} INR")
+        print(f"{amount} {currency.upper()} = {result} USD")
 
 else:
     print("Convertion criterion did not match")
